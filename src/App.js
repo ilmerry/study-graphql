0c2e42eb83f6e8f2d6ea387fb0bc565dc6c05907
Eunhyung Choi <milkiss0414@naver.com>
2023-05-22 18:25:20 +0900

    Refact: DisplayLocations 컴포넌트 분리

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useQuery, gql } from "@apollo/client";
+import DisplayLocations from "./components/DisplayLocations";
 
 function App() {
   const GET_LOCATIONS = gql`
@@ -11,32 +12,9 @@ function App() {
       }
     }
   `;
-
-  function DisplayLocations() {
-    const { loading, error, data } = useQuery(GET_LOCATIONS);
-
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p>Error : {error.message}</p>;
-
-    return data.locations.map(({ id, name, description, photo }) => (
-      <div key={id}>
-        <h3>{name}</h3>
-        <img
-          width="400"
-          heigh="250"
-          alt="location-reference"
-          src={`${photo}`}
-        />
-        <br />
-        <b>About this location:</b>
-        <p>{description}</p>
-        <br />
-      </div>
-    ));
-  }
   return (
     <div>
-      <DisplayLocations />
+      <DisplayLocations locations={GET_LOCATIONS} />
     </div>
   );
 }
--- src/components/DisplayLocations.jsx
+++ src/components/DisplayLocations.jsx
@@ -1,5 +1,20 @@
+import { useQuery } from "@apollo/client";
 import React from "react";
 
-export default function DisplayLocations() {
-  return <div>DisplayLocations</div>;
+export default function DisplayLocations(locations) {
+  const { loading, error, data } = useQuery(locations);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
+
+  return data.locations.map(({ id, name, description, photo }) => (
+    <div key={id}>
+      <h3>{name}</h3>
+      <img width="400" heigh="250" alt="location-reference" src={`${photo}`} />
+      <br />
+      <b>About this location:</b>
+      <p>{description}</p>
+      <br />
+    </div>
+  ));
 }
