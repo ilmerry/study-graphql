9d6436da8b91b9a7f988cbcf2495785b91d32c6e
Eunhyung Choi <milkiss0414@naver.com>
2023-05-22 18:23:45 +0900

    Feat: useQuery

--- src/App.js
+++ src/App.js
@@ -1,5 +1,44 @@
+import { useQuery, gql } from "@apollo/client";
+
 function App() {
-  return <div></div>;
+  const GET_LOCATIONS = gql`
+    query GetLocations {
+      locations {
+        id
+        name
+        description
+        photo
+      }
+    }
+  `;
+
+  function DisplayLocations() {
+    const { loading, error, data } = useQuery(GET_LOCATIONS);
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error : {error.message}</p>;
+
+    return data.locations.map(({ id, name, description, photo }) => (
+      <div key={id}>
+        <h3>{name}</h3>
+        <img
+          width="400"
+          heigh="250"
+          alt="location-reference"
+          src={`${photo}`}
+        />
+        <br />
+        <b>About this location:</b>
+        <p>{description}</p>
+        <br />
+      </div>
+    ));
+  }
+  return (
+    <div>
+      <DisplayLocations />
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
 const root = ReactDOM.createRoot(document.getElementById("root"));
+const client = new ApolloClient({
+  uri: "https://flyby-router-demo.herokuapp.com/",
+  cache: new InMemoryCache(),
+});
+
+client
+  .query({
+    query: gql`
+      query GetLocations {
+        locations {
+          id
+          name
+          description
+          photo
+        }
+      }
+    `,
+  })
+  .then((result) => console.log(result));
+
 root.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>
+  </ApolloProvider>
 );
